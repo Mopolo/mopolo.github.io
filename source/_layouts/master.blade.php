fcfb0c735df781924037885d74d1a310103f6a61
Nathan Boiron <nathan.boiron@gmail.com>
2017-09-14 22:27:30 +0200

    basic layout

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700">
+    </head>
+    <body>
+        <div class="container">
+            <div class="sidebar">
+                <h1>
+                    Nathan<br />
+                    Boiron
+                </h1>
+
+                <h2>Web developer</h2>
+
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/experience">Experience</a>
+                    <a class="active" href="/contact">Contact</a>
+                    <a href="/work">Work</a>
+                    <a href="/projects">Projects</a>
+                    <a href="/games">Games</a>
+                    <a href="https://github.com/Mopolo">Github</a>
+                </nav>
+
+                <footer>
+<pre class="normal" id="duck">
+  _
+<span id="duckface">-</span>(.)__
+ (___/
+</pre>
+                </footer>
+            </div>
+
+            <div class="content">
+                @yield('body')
+            </div>
+        </div>
+    </body>
+</html>
---
+++ source/index.blade.php
@@ -0,0 +1,7 @@
+@extends('_layouts.master')
+
+@section('body')
+<article>
+    <h1>Hello world!</h1>
+</article>
+@endsection
