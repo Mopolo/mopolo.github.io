4d3721a999fac6aafc4cbd6f3d9463de2a8dd5fc
Nathan Boiron <nathan.boiron@gmail.com>
2021-03-23 19:20:07 +0100

    Add a skill section in the dataset

--- src/Definition/Cv.php
+++ src/Definition/Cv.php
@@ -11,6 +11,9 @@ final class Cv
     public Contact $contact;
     public Links $links;
 
+    /** @var Skill[] */
+    public array $skills;
+
     /** @var Work[] */
     public array $work;
 
@@ -19,20 +22,4 @@ final class Cv
 
     /** @var School[] */
     public array $studies;
-
-    /**
-     * @param Contact $contact
-     * @param Links $links
-     * @param Work[] $work
-     * @param Project[] $projects
-     * @param School[] $studies
-     */
-    public function __construct(Contact $contact, Links $links, array $work, array $projects, array $studies)
-    {
-        $this->contact = $contact;
-        $this->links = $links;
-        $this->work = $work;
-        $this->projects = $projects;
-        $this->studies = $studies;
-    }
 }
---
+++ src/Definition/Skill.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Mopolo\Cv\Definition;
+
+final class Skill
+{
+    public string $name;
+    public int $level;
+}
