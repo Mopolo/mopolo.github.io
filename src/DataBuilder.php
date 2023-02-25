e7511276c4d1d5957e15484fb1bdda144c02e7e4
Nathan Boiron <nathan.boiron@gmail.com>
2023-02-25 18:14:42 +0100

    wip compatibility

--- src/DataBuilder.php
+++ src/DataBuilder.php
@@ -5,33 +5,19 @@ namespace Mopolo\Cv;
 
 use CuyZ\Valinor\Mapper\Source\Source;
 use CuyZ\Valinor\MapperBuilder;
-use Mopolo\Cv\Binding\HighlightsBinding;
-use Mopolo\Cv\Binding\ImageBinding;
-use Mopolo\Cv\Binding\StringBinding;
 use Mopolo\Cv\Definition\Data;
-use Mopolo\Cv\Support\Translator;
+use Mopolo\Cv\Definition\Highlights;
+use Mopolo\Cv\Definition\Image;
 use SplFileObject;
 
 final class DataBuilder
 {
-    private StringBinding $stringBinding;
-    private ImageBinding $imageBinding;
-    private HighlightsBinding $highlightsBinding;
-
-    public function __construct(Translator $translator)
-    {
-        $this->stringBinding = new StringBinding($translator);
-        $this->imageBinding = new ImageBinding();
-        $this->highlightsBinding = new HighlightsBinding($translator);
-    }
-
     public function build(): Data
     {
         return (new MapperBuilder())
             ->supportDateFormats('Y-m-d')
-            ->alter($this->stringBinding)
-            ->registerConstructor($this->imageBinding)
-            ->registerConstructor($this->highlightsBinding)
+            ->registerConstructor(Image::fromKey(...))
+            ->registerConstructor(Highlights::fromText(...))
             ->mapper()
             ->map(
                 Data::class,
--- src/Definition/Highlights.php
+++ src/Definition/Highlights.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace Mopolo\Cv\Definition;
 
+use function array_map;
+use function explode;
+use function trim;
+
 final class Highlights
 {
     /**
@@ -11,5 +15,11 @@ final class Highlights
     public function __construct(
         public readonly array $lines,
     ) {
+    }
+
+    /** @psalm-pure */
+    public static function fromText(string $lines): Highlights
+    {
+        return new Highlights(array_map(fn(string $line) => trim($line), explode("\n", $lines)));
     }
 }
